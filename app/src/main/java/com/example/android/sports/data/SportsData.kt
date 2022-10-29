1132a6b1b1ebf42232fea76cf340b4fd92ef4f36
Magus Dev <114507764+theMagusDev@users.noreply.github.com>
2022-10-29 15:34:22 +0300

    first commit

--- app/src/main/java/com/example/android/sports/data/SportsData.kt
+++ app/src/main/java/com/example/android/sports/data/SportsData.kt
@@ -23,6 +23,7 @@ import com.example.android.sports.model.Sport
  * Sports data
  */
 object SportsData{
+
     fun getSportsData(): ArrayList<Sport> {
         return arrayListOf(
             Sport(
